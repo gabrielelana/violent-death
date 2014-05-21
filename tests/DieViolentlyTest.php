2cf2256c40b0c3f1f30524999983f9d3bbab06c6
gabriele.lana <gabriele.lana@cleancode.it>
2014-05-21 23:26:16 +0200

    Integration test to confirm segmenation fault on specific platform

--- tests/DieViolentlyTest.php
+++ tests/DieViolentlyTest.php
@@ -19,4 +19,22 @@ class DieViolentlyTest extends PHPUnit_Framework_TestCase
     {
         $this->assertContains('die_violently_after', get_extension_funcs('blast'));
     }
+
+    public function testItWillRaiseSegmentationFault()
+    {
+        $descriptors = [
+            0 => ['pipe', 'r'],
+            1 => ['pipe', 'w'],
+            2 => ['pipe', 'w'],
+        ];
+        $cwd = __DIR__ . '/../examples';
+        $process = proc_open('php die_violently.php', $descriptors, $pipes, $cwd);
+        $stdout = stream_get_contents($pipes[1]);
+        $stderr = stream_get_contents($pipes[2]);
+        fclose($pipes[1]);
+        fclose($pipes[2]);
+        $status = proc_get_status($process);
+        $this->assertTrue($status['signaled']);
+        $this->assertEquals(11, $status['termsig']);
+    }
 }
